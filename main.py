4ec79f79de682dbcdd77dbde76cbcb61e40a1797
Lukasz Zarnowski <lukasz.j.zarnowski@gmail.com>
2021-06-20 17:02:34 +0200

    Aktualizacja pliku o nowe funkcje

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import random
 import pygame
 import pygame.freetype
+import click
+from click import INT
 
 pygame.init()
 
@@ -8,10 +10,11 @@ pygame.init()
 czarny = (0, 0, 0)
 bialy = (255, 255, 255)
 szary = (200, 200, 200)
+niebieski = (0, 231, 205)
 losowy_kolor = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 # Stworzenie klasy obiektu (pojedynczy "pixel", wykorzystywany pozniej do tworzenia weza, cukierkow oraz kamieni
-class kwadrat(object):
+class Kwadrat:
 
     def __init__(self, start, kolor=losowy_kolor):
         self.kolor = kolor
@@ -28,20 +31,20 @@ class kwadrat(object):
     liczba_boxow_w_rzedzie = 22
     wymiary_planszy_xy = 500
 
-    def rysuj(self, powierzchnia, x=False):
+    def rysuj(self, powierzchnia):
         dis = self.wymiary_planszy_xy // self.liczba_boxow_w_rzedzie
         i = self.pos[0]
         j = self.pos[1]
         pygame.draw.rect(powierzchnia, self.kolor, (i*dis, j*dis, dis, dis))
 
 
-class snake(object):
+class Snake:
     cialo = []
     zmiana_kierunku = {}
 
-    def __init__(self, color, pos):
-        self.color = color
-        self.glowa = kwadrat(pos)
+    def __init__(self, kolor, pos):
+        self.kolor = kolor
+        self.glowa = Kwadrat(pos)
         self.cialo.append(self.glowa)
         self.os_x = 0
         self.os_y = 1
@@ -51,7 +54,7 @@ class snake(object):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-            for v in pygame.key.get_pressed():
+            for _ in pygame.key.get_pressed():
                 if pygame.key.get_pressed()[pygame.K_UP]:
                     if self.os_x == 0 and self.os_y == -1:
                         pass
@@ -101,7 +104,7 @@ class snake(object):
                     c.ruch(c.os_x, c.os_y)
 
     def poczatek(self, pozycja):
-        self.glowa = kwadrat(pozycja)
+        self.glowa = Kwadrat(pozycja)
         self.cialo = []
         self.cialo.append(self.glowa)
         self.zmiana_kierunku = {}
@@ -113,10 +116,10 @@ class snake(object):
         tail = self.cialo[-1]
         dx, dy = tail.os_x, tail.os_y
 
-        if dx == 1 and dy == 0: self.cialo.append(kwadrat((tail.pos[0] - 1, tail.pos[1])))
-        elif dx == -1 and dy == 0: self.cialo.append(kwadrat((tail.pos[0] + 1, tail.pos[1])))
-        elif dx == 0 and dy == 1: self.cialo.append(kwadrat((tail.pos[0], tail.pos[1] - 1)))
-        elif dx == 0 and dy == -1: self.cialo.append(kwadrat((tail.pos[0], tail.pos[1] + 1)))
+        if dx == 1 and dy == 0: self.cialo.append(Kwadrat((tail.pos[0] - 1, tail.pos[1])))
+        elif dx == -1 and dy == 0: self.cialo.append(Kwadrat((tail.pos[0] + 1, tail.pos[1])))
+        elif dx == 0 and dy == 1: self.cialo.append(Kwadrat((tail.pos[0], tail.pos[1] - 1)))
+        elif dx == 0 and dy == -1: self.cialo.append(Kwadrat((tail.pos[0], tail.pos[1] + 1)))
 
         self.cialo[-1].os_x = dx
         self.cialo[-1].os_y = dy
@@ -124,24 +127,24 @@ class snake(object):
     def rysuj(self, powierzchnia):
         for i, c in enumerate(self.cialo):
             if i == 0:
-                c.rysuj(powierzchnia, True)
+                c.rysuj(powierzchnia)
             else:
                 c.rysuj(powierzchnia)
 
 
 def rysuj_okno_ponownie(powierzchnia):
-    global snake, cukierek, kamien, kamien1, kamien2
+    global Snake, cukierek, kamienie
     powierzchnia.fill(bialy)
-    snake.rysuj(powierzchnia)
+    Snake.rysuj(powierzchnia)
     cukierek.rysuj(powierzchnia)
-    kamien.rysuj(powierzchnia)
-    kamien2.rysuj(powierzchnia)
-    kamien2.rysuj(powierzchnia)
-    kamien3.rysuj(powierzchnia)
+    for kamien in kamienie:
+        kamien.rysuj(powierzchnia)
     pygame.display.update()
 
 def losowanie_cukierka(wymiary_planszy_xy, item):
     positions = item.cialo
+    x = wymiary_planszy_xy
+
 
     while True:
         x = random.randrange(wymiary_planszy_xy)
@@ -168,6 +171,7 @@ def losowanie_kamienia(wymiary_planszy_xy, item_k):
 
     return (x, y)
 
+
 def komunikat(wymiary_planszy_xy):
     screen = pygame.display.set_mode((wymiary_planszy_xy, wymiary_planszy_xy))
     GAME_FONT = pygame.freetype.SysFont("monospace", 12)
@@ -178,32 +182,37 @@ def komunikat(wymiary_planszy_xy):
             if event.type == pygame.QUIT:
                 running = False
 
-            #elif pygame.key.get_pressed()[pygame.K_r]:
-                #okno = pygame.display.set_mode((wymiary_planszy_xy, wymiary_planszy_xy))
-                #rysuj_okno_ponownie(okno)
-                #snake.poczatek(random_kolor, (10, 10))
-                #main()
-
 
         screen.fill(szary)
-        text_surface, rect = GAME_FONT.render('Koniec gry, osiagnięty wynik to: ' + str(len(snake.cialo) - 1) + ' cukierki/ow', (0, 0, 0))
+        text_surface, rect = GAME_FONT.render('Koniec gry, osiagnięty wynik to: ' + str(len(Snake.cialo) - 1) + ' cukierki/ow', (0, 0, 0))
         text_surface2, rect = GAME_FONT.render('Uruchom program ponownie by zagrać jeszcze raz', czarny)
         screen.blit(text_surface, (40, 40))
         screen.blit(text_surface2, (40, 80))
         pygame.display.flip()
 
 
-def main():
-    global snake, cukierek, kamien, kamien2, kamien3, text_surface
+@click.command(
+    help="Snake game"
+)
+@click.option(
+    "--rock_number",
+    type=INT,
+    default=3,
+    help="Number of rocks to appear on the board.",
+)
+def main(rock_number):
+    global Snake, cukierek, kamienie, text_surface, liczba_kamieni
+    liczba_kamieni = rock_number
     wymiary_planszy_xy = 500
     liczba_boxow_w_rzedzie = 22
     tempo_gry = 12
     okno = pygame.display.set_mode((wymiary_planszy_xy, wymiary_planszy_xy))
-    snake = snake(losowy_kolor, (10, 10))
-    cukierek = kwadrat(losowanie_cukierka(liczba_boxow_w_rzedzie, snake), kolor=losowy_kolor)
-    kamien = kwadrat(losowanie_kamienia(liczba_boxow_w_rzedzie, snake), kolor=czarny)
-    kamien2 = kwadrat(losowanie_kamienia(liczba_boxow_w_rzedzie, snake), kolor=czarny)
-    kamien3 = kwadrat(losowanie_kamienia(liczba_boxow_w_rzedzie, snake), kolor=czarny)
+    Snake = Snake(losowy_kolor, (10, 10))
+    cukierek = Kwadrat(losowanie_cukierka(liczba_boxow_w_rzedzie, Snake), kolor=niebieski)
+    kamienie = [
+        Kwadrat(losowanie_kamienia(liczba_boxow_w_rzedzie, Snake), kolor=czarny)
+        for _ in range(liczba_kamieni)
+    ]
     gra = True
 
     zegar = pygame.time.Clock()
@@ -211,36 +220,31 @@ def main():
     while gra:
         pygame.time.delay(20)
         zegar.tick(tempo_gry)
-        snake.ruch()
-        if snake.cialo[0].pos == cukierek.pos:
-            snake.dodaj_kwadrat()
-            cukierek = kwadrat(losowanie_cukierka(liczba_boxow_w_rzedzie, snake), kolor=(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
-            #kamien = kwadrat(losowanie_kamienia(liczba_boxow_w_rzedzie, snake), kolor=czarny)
-            #kamien2 = kwadrat(losowanie_kamienia(liczba_boxow_w_rzedzie, snake), kolor=czarny)
-            #kamien3 = kwadrat(losowanie_kamienia(liczba_boxow_w_rzedzie, snake), kolor=czarny)
-            print("Twoj aktualny wynik to: " + str(len(snake.cialo) - 1))
-            print(snake.cialo)
+        Snake.ruch()
+        if Snake.cialo[0].pos == cukierek.pos:
+            Snake.dodaj_kwadrat()
+            cukierek = Kwadrat(losowanie_cukierka(liczba_boxow_w_rzedzie, Snake), kolor=niebieski)
+            for kamien in kamienie:
+                kamien.pos = losowanie_kamienia(liczba_boxow_w_rzedzie, Snake)
+            print("Twoj aktualny wynik to: " + str(len(Snake.cialo) - 1))
 
-        for x in range(len(snake.cialo)):
-            if snake.cialo[x].pos in list(map(lambda z: z.pos, snake.cialo[x + 1:])):
-                print("Koniec gry", 'Przegrales/as, zacznij jeszcze raz... osiagnięty wynik to: ' + str(len(snake.cialo) - 1) + ' cukierki/ow')
+        for x in range(len(Snake.cialo)):
+            if Snake.cialo[x].pos in list(map(lambda z: z.pos, Snake.cialo[x + 1:])):
+                print("Koniec gry", 'Przegrales/as, zacznij jeszcze raz... osiagnięty wynik to: ' + str(len(Snake.cialo) - 1) + ' cukierki/ow')
 
                 okno.blit(komunikat(wymiary_planszy_xy), (40, 40))
-                #snake.poczatek(losowy_kolor, (10, 10))
+                Snake.poczatek(losowy_kolor, (10, 10))
 
-            elif snake.cialo[x].pos == kamien.pos or snake.cialo[x].pos == kamien2.pos or snake.cialo[x].pos == kamien3.pos:
-                print("Koniec gry", 'Wpadles w kamien i przegrales/as, zacznij jeszcze raz... osiagnięty wynik to: ' + str(len(snake.cialo) - 1) + ' cukierki/ow')
+            elif Snake.cialo[x].pos in (k.pos for k in kamienie):
+                print("Koniec gry", 'Wpadles w kamien i przegrales/as, zacznij jeszcze raz... osiagnięty wynik to: ' + str(len(Snake.cialo) - 1) + ' cukierki/ow')
 
                 okno.blit(komunikat(wymiary_planszy_xy), (40, 40))
-                #snake.poczatek(losowy_kolor, (10, 10))
+                Snake.poczatek(losowy_kolor, (10, 10))
                 break
-
 
         rysuj_okno_ponownie(okno)
 
-    pass
 
-# Wartosci liczby boxow w rzedzie (rozmiaru planszy, nie okna) konieczne rowniez w klasie kwadrat
 if __name__ == '__main__':
     main()
 
